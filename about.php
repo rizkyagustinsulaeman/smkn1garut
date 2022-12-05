5ae1deefb40a97251bc8c62e73c3ea333c8ac50d
rizkyagustinsulaeman <agustinsulaemanrizky@gmail.com>
2022-12-05 08:57:44 +0700

    menambahkan file

---
+++ about.php
@@ -0,0 +1,257 @@
+<?= $this->extend('template'); ?>
+
+<?= $this->section('content'); ?>
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container">
+
+            <ol>
+                <li><a href="index.html">Home</a></li>
+                <li>About</li>
+            </ol>
+
+
+    </section><!-- End Breadcrumbs -->
+
+    <div class="lecturers-page-area">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+
+                    <div class="lecturers-contact-info"> <img src="assets/img/portfolio/0.jpg" class="img-fluid" alt="" width="600">
+
+                        <p1 style="color:black">Kepala Sekolah SMKN 1 Garut</p1>
+
+
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
+                    <div class="section-title">
+                        <h2>Sambutan Bapak Kepala Sekolah</h2>
+                        <p></p>
+                    </div>
+
+                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+
+                        <p>Bismillahirrahmaanirrahiim <br>
+                            Assalaamualaikum Wr. Wb. <br>
+                            Penerimaan Peserta Didik baru SMKN 1 GARUT tahun pelajaran 2021/2022 akan segera dimulai, kami berkomitmen untuk melaksanakan PPDB dengan azas <br>
+                            1. Objektif <br>
+                            2. Transparan <br>
+                            3. Akuntabel <br>
+                            Kami memiliki tujuan untuk memperluas akses layanan pendidikan serta melakukan pemerataan mutu pendidikan Jadi siapkan diri kalian, dan tetap dirumah saja, SMK Bisa!!</p>
+
+
+                    </div>
+                </div>
+            </div>
+
+            <br>
+            <br>
+            <br>
+
+            <div class="container" data-aos="fade-up">
+
+                <div class="section-title">
+                    <h3>Daftar Wakil Kepala Sekolah Beserta Staf Wakil Kepala Sekolah</h3>
+                    <p>Dalam Melaksanakan Tugasnya Bapak Kepala Sekolah Dibantu Oleh Wakil Kepala Sekolah Beserta Staf nya.</p>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-2">
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="pic"><img src="assets/img/portfolio/iwan.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+     width: 200px;
+     height: 200px;
+     overflow: hidden;
+     border-radius: 50%;
+     margin: auto;
+     "></div>
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="member-info"> </div>
+                        <h5>Iwan Ridwan, S.Sy., M.Si.</h5>
+                        <p>Staff Waka Bidang Humas</p>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="pic"><img src="assets/img/portfolio/nurdin.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+     width: 200px;
+     height: 200px;
+     overflow: hidden;
+     border-radius: 50%;
+     margin: auto;
+     "></div>
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="member-info"> </div>
+                        <h5>Nurdin Rusli, S.T. M.Pd.</h5>
+                        <p>Wakasek Bidang Kesiswaan
+                        </p>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="pic"><img src="assets/img/portfolio/heri.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+     width: 200px;
+     height: 200px;
+     overflow: hidden;
+     border-radius: 50%;
+     margin: auto;
+     "></div>
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="member-info"> </div>
+                        <h5>Heri Wahyudianto, S.Pd.</h5>
+                        <p>Wakasek Bidang Sarana Prasarana</p>
+                    </div>
+
+                    <div class="container" data-aos="fade-up">
+
+
+                        <div class="row">
+                            <div class="col-lg-2">
+                                <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                                </div>
+
+                                <div class="pic"><img src="assets/img/portfolio/fitri.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+width: 200px;
+height: 200px;
+overflow: hidden;
+border-radius: 50%;
+margin: auto;
+"></div>
+                                <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                                </div>
+
+                                <div class="member-info"> </div>
+                                <h5>Fitriani Indah Lestari, S.Si., M.Pd.</h5>
+                                <p>Staff Waka Bidang Sarana Prasarana</p>
+                            </div>
+                            <div class="col-lg-2">
+                                <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                                </div>
+
+                                <div class="pic"><img src="assets/img/portfolio/reni.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+width: 200px;
+height: 200px;
+overflow: hidden;
+border-radius: 50%;
+margin: auto;
+"></div>
+                                <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                                </div>
+
+                                <div class="member-info"> </div>
+                                <h5>Reni Nurhaerani, S.Pd.</h5>
+                                <p>Wakasek Bidang Manajemen Mutu
+                                </p>
+                            </div>
+                            <div class="col-lg-2">
+                                <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                                </div>
+
+                                <div class="pic"><img src="assets/img/portfolio/hani.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+width: 200px;
+height: 200px;
+overflow: hidden;
+border-radius: 50%;
+margin: auto;
+"></div>
+                                <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                                </div>
+
+                                <div class="member-info"> </div>
+                                <h5>Hani Ariani, M.Pd.</h5>
+                                <p> Wakasek Bidang Kurikulum</p>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-lg-2">
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="pic"><img src="assets/img/portfolio/ardi.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+     width: 200px;
+     height: 200px;
+     overflow: hidden;
+     border-radius: 50%;
+     margin: auto;
+     "></div>
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="member-info"> </div>
+                        <h5>Ardi Hardiansyah, S.Si.</h5>
+                        <p>Staff Waka Bidang Kesiswaan</p>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="pic"><img src="assets/img/portfolio/dede.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+     width: 200px;
+     height: 200px;
+     overflow: hidden;
+     border-radius: 50%;
+     margin: auto;
+     "></div>
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="member-info"> </div>
+                        <h5>Dede Rohman, S.Pd., S.Kom., M.Kom.</h5>
+                        <p>Staff Waka Bidang Kurikulum
+                        </p>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="pic"><img src="assets/img/portfolio/usman.png" class="img-fluid" alt="" class="img-responsive" alt="team" style="border: 2px solid #000080;
+     width: 200px;
+     height: 200px;
+     overflow: hidden;
+     border-radius: 50%;
+     margin: auto;
+     "></div>
+                        <div class="col-lg-1 col-md-4 col-6 d-flex align-items-center justify-content-center">
+                        </div>
+
+                        <div class="member-info"> </div>
+                        <h5>Usman, S.Pd.</h5>
+                        <p>Staff Waka Bidang Lingkungan Hidup</p>
+                    </div>
+
+                    <div class="container" data-aos="fade-up">
+
+
+                    </div>
+
+                </div>
+
+            </div>
+            </section>
+
+</main><!-- End #main -->
+
+
+<?= $this->endSection(); ?>
